4cd0301e33146a0e1b18cf4c15ee9f99a09f30b5
Michel Vollebregt <mi.vollebregt@gmail.com>
2018-12-04 22:06:04 +0100

    Locatie detecteren en tonen op scherm

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AlertController} from '@ionic/angular';
+import {Plugins} from '@capacitor/core';
+
+const { Geolocation } = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -7,4 +11,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  constructor(private alertController: AlertController) {
+  }
+
+  async onVoegHuidigeLocatieToeClicked() {
+    console.log('click');
+    const coordinates = await Geolocation.getCurrentPosition();
+    console.log(coordinates);
+    const alert = await this.alertController.create({
+      header: 'Coordinaten',
+      message: `${coordinates.coords.latitude} / ${coordinates.coords.longitude}`
+    });
+    console.log('alert?');
+    alert.present();
+  }
 }
