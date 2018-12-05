893100f65bbda493afb31bde23d61519974fb400
Michel Vollebregt <mi.vollebregt@gmail.com>
2018-12-05 20:31:52 +0100

    Geofence toevoegen

---
+++ src/app/shared/geofence.service.ts
@@ -0,0 +1,34 @@
+import {Injectable} from '@angular/core';
+import {Geofence} from '@ionic-native/geofence/ngx';
+import * as moment from 'moment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeofenceService {
+
+  // TODO: http://invisibleloop.com/troubleshooting-ionic-native-geofence-plugin-build-issues/
+
+  constructor(private geofence: Geofence, private storage: Storage) {
+    this.geofence.initialize().then(
+      // resolved promise does not return a value
+      () => console.log('Geofence Plugin Ready'),
+      (err) => console.log(err)
+    );
+  }
+
+  async addLocation(location: { id: string, latitude: number, longitude: number }) {
+    await this.geofence.addOrUpdate({
+      ...location, radius: 100, transitionType: 3
+    });
+    this.geofence.onTransitionReceived().subscribe((geofences: any[]) =>
+      geofences.forEach(geofence => this.transitionReceived(geofence)));
+  }
+
+  private async transitionReceived(geofence: {transitionType: number}) {
+    const log = await this.storage.getItem('log');
+    const time = moment().format('dd DD MMM HH:mm');
+    const status = geofence.transitionType === 1 ? 'Entered' : 'Exited';
+    this.storage.setItem('log', `${log}\n${time} ${status}`);
+  }
+}
